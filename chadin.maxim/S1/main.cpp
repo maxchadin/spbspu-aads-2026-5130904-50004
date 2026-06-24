a803cf12a8766842960b78cf7a3c6d74ed965dd6
maxchadin <maxchadin080418@gmail.com>
2026-06-24 21:32:58 +0300

    fix3 main.cpp

--- chadin.maxim/S1/main.cpp
+++ chadin.maxim/S1/main.cpp
@@ -1,132 +1,129 @@
 #include <iostream>
 #include <string>
 #include <limits>
+#include <cctype>
 #include "BiList.hpp"
 
 int main()
 {
   using namespace chadin;
 
-  try
+  BiList< std::pair< std::string, BiList< unsigned long long > > > data;
+  std::string name;
+
+  while (std::cin >> name)
   {
-    BiList< std::pair< std::string, BiList< unsigned long long > > > data;
-    std::string name = "";
-
-    while (std::cin >> name)
+    BiList< unsigned long long > numbers;
+    char c;
+    while (std::cin.get(c))
     {
-      BiList< unsigned long long > numbers;
-      while (std::cin.peek() != '\n' && std::cin.peek() != EOF)
+      if (c == '\n')
       {
-        unsigned long long num = 0;
-        if (std::cin >> num)
-        {
-          numbers.pushBack(num);
-        }
-        else
-        {
-          std::cin.clear();
-          while (std::cin.peek() != '\n' && std::cin.peek() != EOF && !std::isdigit(std::cin.peek()))
-          {
-            std::cin.ignore();
-          }
-        }
+        break;
       }
-      data.pushBack({name, std::move(numbers)});
-    }
-
-    if (data.isEmpty())
-    {
-      std::cout << "0\n";
-      return 0;
-    }
-
-    size_t nameCount = 0;
-    for (auto it = data.cbegin(); it != data.cend(); ++it)
-    {
-      std::cout << it->first;
-      if (++nameCount < data.getSize())
+      if (std::isspace(c))
       {
-        std::cout << " ";
+        continue;
       }
+
+      std::cin.putback(c);
+      unsigned long long num = 0;
+
+      if (std::cin >> num)
+      {
+        numbers.pushBack(num);
+      }
+      else
+      {
+        std::cin.clear();
+        std::cin.ignore();
+      }
+    }
+    data.pushBack({name, std::move(numbers)});
+  }
+
+  if (data.isEmpty())
+  {
+    std::cout << "0\n";
+    return 0;
+  }
+
+  size_t maxLen = 0;
+  for (auto it = data.cbegin(); it != data.cend(); )
+  {
+    std::cout << it->first;
+    if (it->second.getSize() > maxLen)
+    {
+      maxLen = it->second.getSize();
+    }
+    if (++it != data.cend())
+    {
+      std::cout << " ";
+    }
+  }
+  std::cout << "\n";
+
+  if (maxLen == 0)
+  {
+    std::cout << "0\n";
+    return 0;
+  }
+
+  BiList< LCIter< unsigned long long > > currentIters;
+  BiList< LCIter< unsigned long long > > endIters;
+
+  for (auto it = data.cbegin(); it != data.cend(); ++it)
+  {
+    currentIters.pushBack(it->second.cbegin());
+    endIters.pushBack(it->second.cend());
+  }
+
+  BiList< unsigned long long > sums;
+
+  for (size_t i = 0; i < maxLen; ++i)
+  {
+    unsigned long long rowSum = 0;
+    bool isFirstInRow = true;
+
+    auto currIt = currentIters.begin();
+    auto endIt = endIters.cbegin();
+
+    for (auto dataIt = data.cbegin(); dataIt != data.cend(); ++dataIt)
+    {
+      if (*currIt != *endIt)
+      {
+        unsigned long long val = **currIt;
+        if (!isFirstInRow)
+        {
+          std::cout << " ";
+        }
+        std::cout << val;
+        isFirstInRow = false;
+
+        if (std::numeric_limits< unsigned long long >::max() - rowSum < val)
+        {
+          std::cerr << "Sum overflow\n";
+          return 1;
+        }
+        rowSum += val;
+        ++(*currIt);
+      }
+      ++currIt;
+      ++endIt;
     }
     std::cout << "\n";
-
-    size_t maxLen = 0;
-    for (auto it = data.cbegin(); it != data.cend(); ++it)
-    {
-      if (it->second.getSize() > maxLen)
-      {
-        maxLen = it->second.getSize();
-      }
-    }
-
-    if (maxLen == 0)
-    {
-      std::cout << "0\n";
-      return 0;
-    }
-
-    BiList< unsigned long long > sums;
-    bool overflowOccurred = false;
-
-    for (size_t i = 0; i < maxLen; ++i)
-    {
-      unsigned long long rowSum = 0;
-      bool isFirstInRow = true;
-      for (auto it = data.cbegin(); it != data.cend(); ++it)
-      {
-        if (i < it->second.getSize())
-        {
-          auto numIt = it->second.cbegin();
-          for (size_t j = 0; j < i; ++j)
-          {
-            ++numIt;
-          }
-
-          if (!isFirstInRow)
-          {
-            std::cout << " ";
-          }
-          std::cout << *numIt;
-          isFirstInRow = false;
-
-          if (!overflowOccurred)
-          {
-            if (std::numeric_limits< unsigned long long >::max() - rowSum < *numIt)
-            {
-              overflowOccurred = true;
-            }
-            else
-            {
-              rowSum += *numIt;
-            }
-          }
-        }
-      }
-      std::cout << "\n";
-      sums.pushBack(rowSum);
-    }
-
-    if (overflowOccurred)
-    {
-      throw std::overflow_error("Sum overflow");
-    }
-
-    size_t sumCount = 0;
-    for (auto it = sums.cbegin(); it != sums.cend(); ++it)
-    {
-      std::cout << *it;
-      if (++sumCount < sums.getSize())
-      {
-        std::cout << " ";
-      }
-    }
-    std::cout << "\n";
+    sums.pushBack(rowSum);
   }
-  catch (const std::exception& e)
+
+  for (auto it = sums.cbegin(); it != sums.cend(); )
   {
-    std::cerr << e.what() << "\n";
-    return 1;
+    std::cout << *it;
+    if (++it != sums.cend())
+    {
+      std::cout << " ";
+    }
   }
+  std::cout << "\n";
+
   return 0;
 }
