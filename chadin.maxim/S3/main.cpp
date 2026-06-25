59d2607cd090b2ffbf7febef19692b83f3e3edbf
maxchadin <maxchadin080418@gmail.com>
2026-06-26 02:24:34 +0300

    update main.cpp

--- chadin.maxim/S3/main.cpp
+++ chadin.maxim/S3/main.cpp
@@ -1,2 +1,46 @@
-int main()
-{}
+#include "commands.hpp"
+#include <iostream>
+#include <fstream>
+#include <string>
+
+int main(int argc, char** argv)
+{
+  if (argc < 2) {
+    std::cerr << "File name not provided.\n";
+    return 1;
+  }
+
+  std::ifstream file(argv[1]);
+  if (!file.is_open()) {
+    std::cerr << "Failed to open file.\n";
+    return 1;
+  }
+
+  try {
+    chadin::GraphTable allGraphs(32, 4);
+    std::string graphName;
+    size_t edgesCount = 0;
+
+    while (file >> graphName >> edgesCount) {
+      chadin::Graph g;
+      for (size_t i = 0; i < edgesCount; ++i) {
+        std::string from, to;
+        unsigned int weight;
+        file >> from >> to >> weight;
+        g.addEdge(from, to, weight);
+      }
+      if (!allGraphs.has(graphName)) {
+        allGraphs.add(graphName, g);
+      } else {
+        allGraphs.get(graphName) = g;
+      }
+    }
+
+    chadin::processCommands(std::cin, std::cout, allGraphs);
+  } catch (const std::exception& e) {
+    std::cerr << e.what() << '\n';
+    return 2;
+  }
+
+  return 0;
+}
