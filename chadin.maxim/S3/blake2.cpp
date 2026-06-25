1c628b576cce7e200c2e634343e6505245642e0c
maxchadin <maxchadin080418@gmail.com>
2026-06-26 01:39:49 +0300

    created blake2.cpp

---
+++ chadin.maxim/S3/blake2.cpp
@@ -0,0 +1,27 @@
+#include "blake2.hpp"
+
+namespace chadin {
+  size_t Blake2sHash::operator()(const std::string& str) const
+  {
+    const uint32_t IV[8] = {
+      0x6A09E667, 0xBB67AE85, 0x3C6EF372, 0xA54FF53A,
+      0x510E527F, 0x9B05688C, 0x1F83D9AB, 0x5BE0CD19
+    };
+    uint32_t hash[8];
+    for (int i = 0; i < 8; ++i) {
+      hash[i] = IV[i];
+    }
+
+    for (size_t i = 0; i < str.length(); ++i) {
+      uint32_t charData = static_cast< uint32_t >(str[i]);
+      hash[i % 8] ^= charData;
+      hash[(i + 1) % 8] += charData;
+    }
+
+    size_t finalHash = 0;
+    for (int i = 0; i < 8; ++i) {
+      finalHash ^= hash[i];
+      finalHash = (finalHash << 5) | (finalHash >> (sizeof(size_t) * 8 - 5));
+    }
+    return finalHash;
+  }
