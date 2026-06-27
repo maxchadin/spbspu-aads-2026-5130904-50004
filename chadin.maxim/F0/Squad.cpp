3aa19b9656530a09d3372f3aaa21f35850368acd
maxchadin <maxchadin080418@gmail.com>
2026-06-27 20:55:44 +0300

    added addPlayer and removePlayer to squad.cpp

--- chadin.maxim/F0/Squad.cpp
+++ chadin.maxim/F0/Squad.cpp
@@ -20,3 +20,28 @@ const std::string& chadin::Squad::getName() const
 {
   return name_;
 }
+
+bool chadin::Squad::addPlayer(const int playerId, const std::string& position)
+{
+  if (hasPlayer(playerId)) {
+    return false;
+  }
+  int slot = getSlotIndex(position);
+  if (slot != -1 && positions_[slot] == -1) {
+    positions_[slot] = playerId;
+    return true;
+  } else {
+    return false;
+  }
+}
+
+bool chadin::Squad::removePlayer(const int playerId)
+{
+  for (int i = 0; i < SQUAD_SIZE; ++i) {
+    if (positions_[i] == playerId) {
+      positions_[i] = -1;
+      return true;
+    }
+  }
+  return false;
+}
