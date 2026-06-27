31624ea1ca4cd25d82742b21903b670cdeba1808
maxchadin <maxchadin080418@gmail.com>
2026-06-28 00:02:44 +0300

    fix acceptace

--- chadin.maxim/S4/commands.cpp
+++ chadin.maxim/S4/commands.cpp
@@ -25,13 +25,17 @@ void processCommands(std::istream& in, std::ostream& out, DictsTree& dicts)
     }
     std::istringstream iss(line);
     std::string cmd;
-    iss >> cmd;
+
+    if (!(iss >> cmd)) {
+      continue;
+    }
 
     if (cmd == "print") {
       std::string name;
+      std::string extra;
       if (iss >> name) {
-        if (!dicts.has(name)) {
-          out << "<EMPTY>\n";
+        if (iss >> extra || !dicts.has(name)) {
+          out << "<INVALID COMMAND>\n";
         } else {
           printDict(out, name, dicts.get(name));
         }
@@ -39,50 +43,61 @@ void processCommands(std::istream& in, std::ostream& out, DictsTree& dicts)
         out << "<INVALID COMMAND>\n";
       }
     } else if (cmd == "complement") {
-      std::string newName, name1, name2;
+      std::string newName, name1, name2, extra;
       if (iss >> newName >> name1 >> name2) {
-        auto d1 = dicts.has(name1) ? dicts.get(name1) : BSTree<int, std::string>();
-        auto d2 = dicts.has(name2) ? dicts.get(name2) : BSTree<int, std::string>();
-        BSTree<int, std::string> dnew;
+        if (iss >> extra || !dicts.has(name1) || !dicts.has(name2)) {
+          out << "<INVALID COMMAND>\n";
+        } else {
+          auto d1 = dicts.get(name1);
+          auto d2 = dicts.get(name2);
+          BSTree<int, std::string> dnew;
 
-        for (auto it = d1.cbegin(); it != d1.cend(); ++it) {
-          if (!d2.has(it->first)) {
-            dnew.push(it->first, it->second);
+          for (auto it = d1.cbegin(); it != d1.cend(); ++it) {
+            if (!d2.has(it->first)) {
+              dnew.push(it->first, it->second);
+            }
           }
+          dicts.push(newName, dnew);
         }
-        dicts.push(newName, dnew);
       } else {
         out << "<INVALID COMMAND>\n";
       }
     } else if (cmd == "intersect") {
-      std::string newName, name1, name2;
+      std::string newName, name1, name2, extra;
       if (iss >> newName >> name1 >> name2) {
-        auto d1 = dicts.has(name1) ? dicts.get(name1) : BSTree<int, std::string>();
-        auto d2 = dicts.has(name2) ? dicts.get(name2) : BSTree<int, std::string>();
-        BSTree<int, std::string> dnew;
+        if (iss >> extra || !dicts.has(name1) || !dicts.has(name2)) {
+          out << "<INVALID COMMAND>\n";
+        } else {
+          auto d1 = dicts.get(name1);
+          auto d2 = dicts.get(name2);
+          BSTree<int, std::string> dnew;
 
-        for (auto it = d1.cbegin(); it != d1.cend(); ++it) {
-          if (d2.has(it->first)) {
-            dnew.push(it->first, it->second);
+          for (auto it = d1.cbegin(); it != d1.cend(); ++it) {
+            if (d2.has(it->first)) {
+              dnew.push(it->first, it->second);
+            }
           }
+          dicts.push(newName, dnew);
         }
-        dicts.push(newName, dnew);
       } else {
         out << "<INVALID COMMAND>\n";
       }
     } else if (cmd == "union") {
-      std::string newName, name1, name2;
+      std::string newName, name1, name2, extra;
       if (iss >> newName >> name1 >> name2) {
-        auto d1 = dicts.has(name1) ? dicts.get(name1) : BSTree<int, std::string>();
-        auto d2 = dicts.has(name2) ? dicts.get(name2) : BSTree<int, std::string>();
-        BSTree<int, std::string> dnew = d1;
-
-        for (auto it = d2.cbegin(); it != d2.cend(); ++it) {
-          if (!dnew.has(it->first)) {
-            dnew.push(it->first, it->second);
+        if (iss >> extra || !dicts.has(name1) || !dicts.has(name2)) {
+          out << "<INVALID COMMAND>\n";
+        } else {
+          auto d1 = dicts.get(name1);
+          auto d2 = dicts.get(name2);
+          BSTree<int, std::string> dnew = d1;
+          for (auto it = d2.cbegin(); it != d2.cend(); ++it) {
+            if (!dnew.has(it->first)) {
+              dnew.push(it->first, it->second);
+            }
           }
+          dicts.push(newName, dnew);
         }
-        dicts.push(newName, dnew);
       } else {
         out << "<INVALID COMMAND>\n";
       }
